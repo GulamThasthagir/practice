9ebdb3cbbe8a95a290688b6c4ea040c39e77b658
Gulam <134676557+GulamThasthagir@users.noreply.github.com>
2023-08-09 12:18:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// let main_div=document.getElementById("main_div")
+// let selection=document.createElement("select")
+// main_div.appendChild(selection)
+// // console.log(main_div)
+// // console.log(selection)
+
+
+// let arr=[]
+// // console.log("qwerty")
+// fetch(` https://pokeapi.co/api/v2/pokemon/`).then((response)=>response.json()).then((data)=>{
+// arr.push(data.results)
+
+// arr.map((item)=>{
+//     // console.log(item);
+//     item.map((item)=>{
+//     let option=document.createElement('option')
+//     option.innerHTML=item.name;
+//     selection.appendChild(option)
+//    })
+//  })
+// })
